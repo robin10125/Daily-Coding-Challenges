6be2b237cbb26446a4d2e897f763c058fc8c4c35
robin10125 <60045773+robin10125@users.noreply.github.com>
2020-10-01 14:01:47 -0400

    Create queen_attack_challenge_new.py
    This solution to the queen's attack challenge uses the method described in the queen_attack_challenge.py file, particularly, it uses vectors to calculate the number of available spaces.  There is a bug in the part that calculates one of the four vectors, the one marked #yis-x vector intersect.  
    Challenge taken from https://www.hackerrank.com/challenges/queens-attack-2/problem?isFullScreen=true

---
+++ queen_attack_challenge_new.py
@@ -0,0 +1,84 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the queensAttack function below.
+def queensAttack(n, k, r_q, c_q, obstacles):
+
+    #queenPosition = [r_q, c_q]
+    closestUpperIntersect = n + 1
+    closestLowerIntersect = 0
+    #vertical vector closest intersect
+    for eachSquare in obstacles:
+        if eachSquare[0] == r_q and eachSquare[1] < closestUpperIntersect and eachSquare[1] - c_q > 0:
+            closestUpperIntersect = eachSquare[1]
+        if eachSquare[0] == r_q and eachSquare[1] > closestLowerIntersect and c_q - eachSquare[1] > 0:
+            closestLowerIntersect = eachSquare[1]
+    verticalLength = closestUpperIntersect - closestLowerIntersect - 2
+    #Number of squares the queen can move to in this line.  
+
+    closestUpperIntersect = n + 1
+    closestLowerIntersect = 0   
+    #horizontal vector closest intersect
+    for eachSquare in obstacles:
+        if eachSquare[1] == c_q and eachSquare[0] < closestUpperIntersect and eachSquare[0] - r_q > 0:
+            closestUpperIntersect = eachSquare[0]
+        if eachSquare[1] == c_q and eachSquare[0] > closestLowerIntersect and r_q - eachSquare[0] > 0:
+            closestLowerIntersect = eachSquare[0]
+    horizontalLength = closestUpperIntersect - closestLowerIntersect - 2
+   
+    closestUpperIntersect = min(n + 1 + (r_q - c_q), n+1)
+    closestLowerIntersect = max(r_q - c_q, 0)
+    #y=x vector closest intersect  Only x direction is needed, since the queen can only travel along a line of slope 1.
+    for eachSquare in obstacles:
+        if (eachSquare[0] - eachSquare[1]) == r_q - c_q and (eachSquare[0] - r_q) / (eachSquare[1] -c_q) == 1:
+            if eachSquare[0] < r_q:
+                closestLowerIntersect = eachSquare[0]
+            if eachSquare[0] > r_q:
+                closestUpperIntersect = eachSquare[0]
+    lineYisXLength = closestUpperIntersect - closestLowerIntersect - 2
+    
+    closestUpperIntersect = min(r_q + c_q, n+1)
+    closestLowerIntersect = max((r_q + c_q) - n - 1, 0)
+    #y=-x vector closest intersect  Only x direction is needed, since the queen can only travel along a line of slope -1.
+    for eachSquare in obstacles:
+        if (eachSquare[0] - eachSquare[1]) == r_q - c_q and (eachSquare[0] - r_q) / (eachSquare[1] -c_q) == -1:
+            if eachSquare[0] < r_q:
+                closestLowerIntersect = eachSquare[0]
+            if eachSquare[0] > r_q:
+                closestUpperIntersect = eachSquare[0]
+    lineYisNegativeXLength = closestUpperIntersect - closestLowerIntersect - 2
+    
+    totalLength = verticalLength + horizontalLength + lineYisXLength + lineYisNegativeXLength
+    return totalLength
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    r_qC_q = input().split()
+
+    r_q = int(r_qC_q[0])
+
+    c_q = int(r_qC_q[1])
+
+    obstacles = []
+
+    for _ in range(k):
+        obstacles.append(list(map(int, input().rstrip().split())))
+
+    result = queensAttack(n, k, r_q, c_q, obstacles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
