fc26579aecb32e3dae7b92894af146fd26fba968
robin10125 <60045773+robin10125@users.noreply.github.com>
2020-10-02 18:26:35 -0400

    Update queen_attack_challenge_new.py
    Fixed the bug described earlier.  Before, the program was checking if the obstacles being stored in the closest intersect variables were indeed closer than the previous.  This meant it was just taking the last 2.  That has been fixed and it now works.

--- queen_attack_challenge_new.py
+++ queen_attack_challenge_new.py
@@ -20,6 +20,7 @@ def queensAttack(n, k, r_q, c_q, obstacles):
             closestLowerIntersect = eachSquare[1]
     verticalLength = closestUpperIntersect - closestLowerIntersect - 2
     #Number of squares the queen can move to in this line.  
+    print(closestLowerIntersect, closestUpperIntersect)
 
     closestUpperIntersect = n + 1
     closestLowerIntersect = 0   
@@ -30,30 +31,36 @@ def queensAttack(n, k, r_q, c_q, obstacles):
         if eachSquare[1] == c_q and eachSquare[0] > closestLowerIntersect and r_q - eachSquare[0] > 0:
             closestLowerIntersect = eachSquare[0]
     horizontalLength = closestUpperIntersect - closestLowerIntersect - 2
-   
+    print(closestLowerIntersect, closestUpperIntersect)
+
     closestUpperIntersect = min(n + 1 + (r_q - c_q), n+1)
     closestLowerIntersect = max(r_q - c_q, 0)
     #y=x vector closest intersect  Only x direction is needed, since the queen can only travel along a line of slope 1.
     for eachSquare in obstacles:
         if (eachSquare[0] - eachSquare[1]) == r_q - c_q and (eachSquare[0] - r_q) / (eachSquare[1] -c_q) == 1:
-            if eachSquare[0] < r_q:
+            print("Found y=x")
+            if eachSquare[0] < r_q and eachSquare[0] > closestLowerIntersect:
                 closestLowerIntersect = eachSquare[0]
-            if eachSquare[0] > r_q:
+            if eachSquare[0] > r_q and eachSquare[0] < closestUpperIntersect:
                 closestUpperIntersect = eachSquare[0]
     lineYisXLength = closestUpperIntersect - closestLowerIntersect - 2
-    
+    print(closestLowerIntersect, closestUpperIntersect)
+
     closestUpperIntersect = min(r_q + c_q, n+1)
     closestLowerIntersect = max((r_q + c_q) - n - 1, 0)
     #y=-x vector closest intersect  Only x direction is needed, since the queen can only travel along a line of slope -1.
     for eachSquare in obstacles:
-        if (eachSquare[0] - eachSquare[1]) == r_q - c_q and (eachSquare[0] - r_q) / (eachSquare[1] -c_q) == -1:
-            if eachSquare[0] < r_q:
+        if (eachSquare[0] + eachSquare[1]) == r_q + c_q and (eachSquare[0] - r_q) / (eachSquare[1] -c_q) == -1:
+            print("Found y=-x")
+            if eachSquare[0] < r_q and eachSquare[0] > closestLowerIntersect:
                 closestLowerIntersect = eachSquare[0]
-            if eachSquare[0] > r_q:
+            if eachSquare[0] > r_q and eachSquare[0] < closestUpperIntersect:
                 closestUpperIntersect = eachSquare[0]
     lineYisNegativeXLength = closestUpperIntersect - closestLowerIntersect - 2
+    print(closestLowerIntersect, closestUpperIntersect)
     
     totalLength = verticalLength + horizontalLength + lineYisXLength + lineYisNegativeXLength
+    print(verticalLength, horizontalLength, lineYisXLength, lineYisNegativeXLength)
     return totalLength
 
 
@@ -82,3 +89,4 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
